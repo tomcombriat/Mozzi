dc4c64ebc6e5ae954a1c7a9f9a24c47bfa91e697
tomcombriat <tomcombriat@live.fr>
2020-08-30 10:45:07 +0200

    minor

--- mozzi_config.h
+++ mozzi_config.h
@@ -91,7 +91,6 @@ You need to have \#define STEREO_HACK true in mozzi_config.h
 */
 #define STEREO_HACK false
 
-<<<<<<< HEAD
 /** @ingroup core
 Defining this option as true in mozzi_config.h allows to completely customize the audio output, e.g. for connecting to external DACs.
 For more detail, @see AudioOuput .
